a23a89d8e5e237f6547270d55b9eb2d776c63e12
iron <skaghdtjr@naver.com>
2026-08-12 11:39:45 +0900

    no message

--- ChatServer.Core/DataBase/DataBaseWorker.cs
+++ ChatServer.Core/DataBase/DataBaseWorker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 using MySqlConnector;
@@ -9,13 +10,13 @@ namespace ChatServer.Core.DataBase
 {
     internal sealed class DbWorker : IAsyncDisposable
     {
-        private readonly int _workerID;
+        private readonly Int32 _workerID;
         private readonly MySqlDataSource _dataSource;
 
         private readonly ConcurrentQueue<IDbJob> _jobQueue = new();
 
         // 작업이 들어올 때 worker를 깨울 신호
-        private readonly SemaphoreSlim _jobSingl = new(0);
+        private readonly SemaphoreSlim _jobSignal = new(0);
 
         private readonly object _stateLock = new object();
 
@@ -49,7 +50,7 @@ namespace ChatServer.Core.DataBase
                 _isStart = true;
                 _acceptingJobs = true;
 
-                //_workerTask = RunAsync();
+                _workerTask = RunAsync();
 
             }
 
@@ -79,16 +80,125 @@ namespace ChatServer.Core.DataBase
 
                 _jobQueue.Enqueue(job);
 
-                _jobSingl.Release();
+
+                // 대기중인 worker 깨움.
+                _jobSignal.Release();
             }
 
             return job.Completion;
         }
 
 
-        public async Task DisoperAsync()
+        public async Task RunAsync()
         {
+            while(true)
+            {
+                // 작업이 들어오거나 Stop 신호가 발생할 때까지 대기.
+                await _jobSignal.WaitAsync();
 
+                if(_jobQueue.TryDequeue(out IDbJob? job))
+                {
+                    // await로 작업 순서 보장
+                    await job.ExcuteAsync(_dataSource, CancellationToken.None);
+
+                    continue;
+                }
+
+                lock(_stateLock)
+                {
+                    // 더 이상 작업을 받지 않고 큐도 비어있으면 종료
+                    if(!_acceptingJobs && _jobQueue.IsEmpty)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"DB Worker {_workerID} 종료");
         }
+
+        public async Task StopAsync()
+        {
+            Task? workerTask;
+
+            lock(_stateLock)
+            {
+                if (!_isStart)
+                    return;
+
+                if(_acceptingJobs)
+                {
+                    _acceptingJobs = false;
+
+                    _jobSignal.Release();
+                }
+
+                workerTask = _workerTask;
+            }
+
+            if(workerTask is not null)
+                await workerTask;
+
+            lock(_stateLock)
+            {
+                _isStart = false;
+                _workerTask = null;
+            }
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_disposed)
+                return;
+
+            await StopAsync();
+
+            _disposed = true;
+            _jobSignal.Dispose();
+        }
+    }
+
+    public sealed class DbWorkerPool
+    {
+        // Worker 관리 리스트
+        private readonly ConcurrentDictionary<Int32, DbWorker> _workerDic = new ConcurrentDictionary<int, DbWorker>();
+        
+        // Worker에 분배하기전 담을 Queue
+        private readonly ConcurrentQueue<IDbJob> _jobQueue = new ConcurrentQueue<IDbJob>();
+
+        private readonly Int32 _maxWorkerCount;
+        private readonly SemaphoreSlim _workerSignal;
+
+        public DbWorkerPool(Int32 wroekrCount, MySqlDataSource dataSource)
+        {
+            _workerSignal = new SemaphoreSlim(wroekrCount, wroekrCount);
+
+            for(int i = 0; i < wroekrCount; i++)
+            {
+                DbWorker _dbWorker = new DbWorker(i, dataSource);
+                if (!_workerDic.TryAdd(_dbWorker.WorkerID, _dbWorker))
+                {
+                    Console.WriteLine($"WorkerDic에 Worker 생성 실패");
+                }
+
+                _dbWorker.start();
+            }
+        }
+
+        public async Task<T> RegisterJob<T>(Func<MySqlConnection, CancellationToken, Task<T>> operation, Int32 workerID, CancellationToken token)
+        {
+            DbWorker? dbWorker;
+
+            if (!_workerDic.TryGetValue(workerID, out dbWorker))
+            {
+                ArgumentNullException.ThrowIfNullOrEmpty($"workerDic에서 TryGetValue 실패 : {workerID}");
+
+                // 여기서 return 시킬게 뭐가 있지?
+            }
+
+            return await dbWorker.EnqueueAsync(operation, token);
+        }
+
+
     }
 }
